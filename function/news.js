b8cc4595e1d0cea2288f21cdfd4b9611f7ee1fa6
bahadirsofuoglu <bahadirsofuoglu@gmail.com>
2021-01-12 11:59:26 +0300

    news api added

--- function/news.js
+++ function/news.js
@@ -3,8 +3,7 @@ const axios = require('axios')
 const fetchNews = async () => {
   try {
     return await axios.get(
-      `http://newsapi.org/v2/top-headlines?country=tr&apiKey=${process.env.NEWS_API_KEY}`,
-      { params: { category: 'sport' } }
+      `http://newsapi.org/v2/top-headlines?country=tr&apiKey=${process.env.NEWS_API_KEY}`
     )
   } catch (error) {
     console.error(error)
--- index.js
+++ index.js
@@ -20,16 +20,16 @@ bot.hears('merhaba alfred', ctx =>
 bot.hears('selamın aleykum', ctx =>
   ctx.reply(`Aleykum Selam comar Efendi ${ctx.from.first_name}`)
 )
-bot.command('alfred', async ctx => {
+bot.hears('!alfred', async ctx => {
   ctx.reply(
     ` Merhaba Efendi ${ctx.from.first_name} acıktınız mı ? Günlük raporu almak isterseniz şunları kullanabilirsiniz:
      Hava Durumu: /havadurumu şehir_ismi
+     Haberler: /haberler 
 yakında daha detaylı bir rapor sunabileceğim..`
   )
 })
 bot.command('havadurumu', async ctx => {
   cityName = ctx.message.text.replace('/havadurumu ', '')
-  console.log(cityName)
   const weather = await fetchWeather(cityName)
   ctx.reply(
     ` Efendi ${ctx.from.first_name} ${cityName} şehrinde hava durumu şöyle:
@@ -40,13 +40,17 @@ bot.command('havadurumu', async ctx => {
 (öhöm pardon bazen İngilizceye kaçabiliyorum)`
   )
 })
-// todo: ı will added news from function folder
-/* bot.command('haberler', async ctx => {
+bot.command('haberler', async ctx => {
   const news = await fetchNews()
   const newsArray = news.data.articles
-  console.log(randomNews(0, newsArray.length - 1))
-  console.log(newsArray[randomNews(0, newsArray.length - 1)])
-}) */
+  const selectedNew = newsArray[randomNews(0, newsArray.length - 1)]
+
+  ctx.reply(
+    ` Efendi ${ctx.from.first_name} gazeteyle uğraşmanızı istemem size güncel haberlerden rastgele birini gösteriyorum:
+      Site: ${selectedNew.url} 
+      `
+  )
+})
 bot.use(ctx => {
   if (ctx.message.text) {
     const message = ctx.message.text
