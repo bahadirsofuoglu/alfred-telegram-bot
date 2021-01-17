371d29c299d6118ed777eab0853271216b9a74e1
bahadirsofuoglu <bahadirsofuoglu@gmail.com>
2021-01-17 18:42:00 +0300

    require telegraf bug fixed

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-const { Composer, Telegraf } = require('micro-bot')
-
 require('dotenv').config()
 const { NODE_ENV, TELEGRAM_BOT_TOKEN_LOCAL } = process.env
+
+const { Composer } = require('micro-bot')
+const { Telegraf } = require('telegraf')
+
 const commands = require('./controllers/commands')
 const hears = require('./controllers/hears')
 const use = require('./controllers/use')
 
 const bot =
   NODE_ENV === 'local' ? new Telegraf(TELEGRAM_BOT_TOKEN_LOCAL) : new Composer()
+
 bot.start(ctx => {
   ctx.reply(
     'Merhaba Ben Alfred !alfred komutunu kullanarak benden yardım isteyebilirsiniz o zamana kadar mutfakta olacağım'
   )
 })
+
 bot.on('photo', ctx => ctx.reply('Fena değil'))
 
 bot.hears('Merhaba alfred', hears.hello)
