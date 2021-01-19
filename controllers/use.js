5182a29c45f6696ee5085522baf469f3e5d6bfd0
bahadirsofuoglu <bahadirsofuoglu@gmail.com>
2021-01-19 17:05:22 +0300

    smile codes added

--- controllers/use.js
+++ controllers/use.js
@@ -1,7 +1,8 @@
+const { isEmoji } = require('../helpers/utils')
 exports.upperCaseControl = ctx => {
   if (ctx.message.text) {
     const message = ctx.message.text
-    if (message === message.toUpperCase()) {
+    if (message === message.toUpperCase() && !isEmoji(message)) {
       ctx.reply(`Efendi ${ctx.from.first_name} lütfen biraz sakin olalım`)
     }
   }
