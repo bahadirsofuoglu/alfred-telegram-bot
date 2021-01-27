f8fb0d1aab33b4754e90356b040cf48ba97ee566
bahadirsofuoglu <bahadirsofuoglu@gmail.com>
2021-01-27 12:11:03 +0300

    news unit testing completed

---
+++ test/unit/helpers/news.test.js
@@ -0,0 +1,33 @@
+const news = require('../../../helpers/news')
+const mockRandom = require('jest-mock-random')
+
+const axios = require('axios')
+jest.mock('axios')
+
+describe('Testing News api', () => {
+  test('is this a function', () => {
+    expect(typeof news.fetchNews).toBe('function')
+  })
+  test('Should be return status ok!', async () => {
+    axios.get.mockResolvedValue({
+      data: {
+        status: 'ok'
+      }
+    })
+    const newsResponse = await news.fetchNews()
+    expect(newsResponse.data.status).toEqual('ok')
+  })
+  test('If throw error', async () => {
+    axios.get.mockReturnValue(undefined)
+    const catchResponse = await news.fetchNews()
+    expect(catchResponse).toEqual(undefined)
+  })
+  test('Random function testing', async () => {
+    const mockRandomNumber = jest.spyOn(Math, 'random')
+    const randomNumber = await news.randomNews(0, 5)
+
+    expect(randomNumber).toBeGreaterThanOrEqual(0)
+    expect(randomNumber).toBeLessThanOrEqual(5)
+    expect(mockRandomNumber).toHaveBeenCalled()
+  })
+})
